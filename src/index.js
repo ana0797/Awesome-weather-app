786a1a51cd0696b521ebf8526f15a59f947fb3a6
ana0797 <121345382+ana0797@users.noreply.github.com>
2023-01-10 19:11:41 +0000

    Upgraded Celsius link

--- src/index.js
+++ src/index.js
@@ -63,6 +63,12 @@ function displayFahrenheitTemperature(event) {
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
 
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celciusTemperature);
+}
+
 let celciusTemperature = null;
 
 let form = document.querySelector("#search-form");
@@ -70,5 +76,8 @@ form.addEventListener("submit", handleSubmit);
 
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Cork");
