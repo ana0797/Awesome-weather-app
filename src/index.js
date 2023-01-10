3588dca7a365cb2b8e13cb5dc09016ee3a753fe0
ana0797 <121345382+ana0797@users.noreply.github.com>
2023-01-10 02:27:41 +0000

    Added a button and a search engine

--- src/index.js
+++ src/index.js
@@ -41,9 +41,16 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
+function search(city) {
+  let apiKey = "9c820d6e689b9a86e1733c7722549767";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-let apiKey = "9c820d6e689b9a86e1733c7722549767";
-let city = "Cork";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
